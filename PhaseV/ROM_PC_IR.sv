67640f79139b39c2137771a12136de58dcc85f89
sudoweilder <robertec2007@gmail.com>
2026-06-04 13:41:00 -0700

    Added a testbench for the upper half of the processor control

--- PhaseV/ROM_PC_IR.sv
+++ PhaseV/ROM_PC_IR.sv
@@ -27,7 +27,53 @@ module ROM_PC_IR(Clk, LD, IR_OUT, UP, Clr);
     IR instructionRegister(Clk, LD, IR_IN, IR_OUT);
 endmodule
 
+/**
+    This testbench tests to see if the connections between the non-controlFSM parts of the control
+    section of the processor communicate with each other. It is already assumed that the modules 
+    inside the ROM_PC_IR module have been tested and work. 
+*/
+module ROM_PC_IR_tb();
+    logic Clk, LD, IR_OUT, UP, Clr;
+    localparam clkTime = 20;
 
+    ROM_PC_IR DUT(Clk, LD, IR_OUT, UP, Clr);
 
+    always begin
+        Clk = 0; #(clkTime/2);
+        Clk = 1; #(clkTime/2);
+    end
 
+    initial begin
+        Clr = 1; LD = 1; UP = 1; 
+        waitCycles(1);
+        assertClear();
 
+        // Tests if value loads from ROM to IR to Output
+        for(int i = 0; i < 256; i++) begin
+            waitCycles(2); // 1 cycle to update teh PC, another to update the 
+            assertIR();
+            assert(DUT.IR_IN == i)
+            else ("Error with ROM connection. Expected ROM output to be %d. Recieved %d", i, DUT.IR_IN);
+        end
+    end
+
+    // waits for a number fo cycles
+    task automatic waitCycles(input cycles);
+        repeat (cycles) begin
+            @(negedge Clk);
+        end 
+    endtask
+
+    // Checks if the clear signal works. It is implied that the clear signal was already running for a 
+    // few cycles. 
+    task automatic assertClear();
+        assert(ROM_PC_IR.PC_addr == 0)
+        else $error("Problem clearing PC Counter. Expected 0, got %d", IR_OUT)
+    endtask
+
+    task automatic assertIR();
+        assert(IR_OUT == DUT.IR_IN)
+        else $error("Problem with IR. IR_IN: %d. IR_OUT: %d", DUT.IR_IN, IR_OUT);
+    endtask
+
+endmodule
