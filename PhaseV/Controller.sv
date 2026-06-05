e8ee3c8fcde944aeb34d58282e13b6a39523aeb5
sudoweilder <robertec2007@gmail.com>
2026-06-04 21:27:17 -0700

    Added controller module

--- PhaseV/Controller.sv
+++ PhaseV/Controller.sv
@@ -3,4 +3,57 @@
 // Project Phase V
 // This file tests the full control side of the processor. 
 
+/**
+    Clk: The clock
+    ResetN: An active-low reset signal
+    D_addr: The memory adress that is being read from  
+    RF_s: Determines whether the register file write data comes from the ALU(0) or the Memory(1)
+    RF_W_addr: the register file address being written to 
+    RF_W_en: when equal to 1, it lets the register file be written to
+    RF_Ra_addr: The register address which data is read from to the RA_data
+                port on the register file
+    RF_Rb_addr: The register address which data is read from to the RB_data port on the register
+                file
+    ALU_s0: The mode that the ALU is in
+*/
+module Controller(  Clk, 
+                    ResetN, 
+                    D_addr, 
+                    RF_s, 
+                    RF_W_addr, 
+                    RF_Ra_addr, 
+                    RF_Rb_addr, 
+                    ALU_s0, 
+                    state, 
+                    nextState, 
+                    IR_OUT);
+    input Clk, ResetN;
+    output [7:0] D_addr;
+    output [3:0] RF_W_addr, RF_Ra_addr, RF_Rb_addr, State, NextState;
+    output [2:0] ALU_s0, RF_W_en, D_wr;
+    output [16:0] IR_OUT;
+    logic LD, UP, Clr; 
+
+    // ROM_PC_IR(Clk, LD, IR_OUT, UP, Clr);
+    ROM_PC_IR instruction(.Clk(Clk), .LD(LD), .UP(UP), .Clr(Clr), .IR_OUT(IR_OUT));
+
+    // ControlFSM(Clk, ResetN, Instruction, PC_clr, PC_up, IR_ld, D_addr, D_wr, RF_s, RF_Ra_addr, 
+                // RF_Rb_addr, RF_W_en, RF_W_addr, ALU_s0)
+    ControlFSM control(
+        .Clk(Clk), 
+        .Instruction(IR_OUT),
+        .ResetN(ResetN),
+        .PC_clr(Clr),
+        .PC_up(UP),
+        .IR_ld(LD),
+        .D_addr(D_addr), 
+        .D_wr(D_wr), 
+        .RF_s(RF_s), 
+        .RF_W_addr(RF_W_addr), 
+        .RF_Ra_addr(RF_Ra_addr),
+        .RF_Rb_addr(RF_Rb_addr),
+        .ALU_s0(ALU_s0), 
+        .State(State), 
+        .nextState(NextState));
+endmodule
 
